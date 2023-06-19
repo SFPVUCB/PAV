2d0a5de81db21b0c6b9513db7dd133ec1ca30e00
Sergio Prudencio <sergexpruve@gmail.com>
2023-06-19 16:16:19 -0400

    Clase numero 10 completada

--- pav/src/app/app.module.ts
+++ pav/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//importamos el modulo de formularios
+import { FormsModule } from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //añadimos a las importaciones el nuevo modulo
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
